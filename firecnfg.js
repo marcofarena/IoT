1f550339a851aad1619b3f160499b116d2b02f5e
marcofarena <63930114+marcofarena@users.noreply.github.com>
2020-06-30 02:50:54 -0300

    Update firecnfg.js

--- firecnfg.js
+++ firecnfg.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDWCEOU324y5NVFKrOQuxw-EgGYx-a8bF0",
+    apiKey: apikey,
     authDomain: "iginuu.firebaseapp.com",
     databaseURL: "https://iginuu.firebaseio.com",
     projectId: "iginuu",
     storageBucket: "iginuu.appspot.com",
-    messagingSenderId: "346974463179",
-    appId: "1:346974463179:web:ec6ef14a7b99aba569a74e"
+    messagingSenderId: "messagesenderid",
+    appId: "appid"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
